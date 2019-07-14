69636a4a28fbc1d4d21ac006779b106d6820591f
alex-eri <alex-eri@users.noreply.github.com>
2019-07-14 18:02:41 +0300

    Update bootstrap.sql

--- sql/bootstrap.sql
+++ sql/bootstrap.sql
@@ -6,3 +6,24 @@ ALTER USER authenticator with password 'pass123';
 ALTER USER dba with password 'adminpass';
 ALTER USER radius with password 'raddpass';
 
+INSERT INTO hotspot.themes (name, variables, label) VALUES ('default', '[{
+"name": "header",
+"label": "Вставка HTML сверху",
+"type": "html"
+},
+{
+"name": "rules",
+"label": "Правила пользования",
+"type": "textarea"
+},
+{
+"name": "title",
+"label": "Заголовок",
+"type": "text"
+},
+{
+"name": "footer",
+"label": "Вставка HTML снизу",
+"type": "html"
+}]
+', 'Простая');
